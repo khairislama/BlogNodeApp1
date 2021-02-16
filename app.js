e153908201da5a4b202609ca2f73c8471e483376
khairislama <khairi.slama@polytechnicien.tn>
2021-02-16 22:54:07 +0100

    finishing the update and delete routes

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const   bodyParser  = require("body-parser"),
-        mongoose    = require("mongoose"),
-        express     = require("express"),
-        app         = express();
+const   bodyParser      = require("body-parser"),
+        mongoose        = require("mongoose"),
+        methodOverride  = require("method-override"),
+        express         = require("express"),
+        app             = express();
 
 // APP CONFIG
 mongoose.connect("mongodb://localhost/restfull_blog_app", {useNewUrlParser: true, useUnifiedTopology: true});
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 // MONGOOSE MODEL CONFIG
 const blogSchema = new mongoose.Schema({
@@ -53,6 +55,36 @@ app.get("/blogs/:id", (req, res)=>{
             res.redirect("/blogs");
         }else{
             res.render("show", {blog: foundBlog});
+        }
+    });
+});
+
+app.get("/blogs/:id/edit", (req, res)=>{
+    Blog.findById(req.params.id, (err, foundBlog)=>{
+        if (err){
+            res.redirect("/blogs");
+        }else {
+            res.render("edit", {blog : foundBlog});
+        }
+    });
+});
+
+app.put("/blogs/:id", (req, res)=>{
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog)=>{
+        if(err){
+            res.redirect("/blogs");
+        }else{
+            res.redirect(`/blogs/${req.params.id}`);
+        }
+    });
+});
+
+app.delete("/blogs/:id", (req, res)=>{
+    Blog.findByIdAndRemove(req.params.id, (err)=>{
+        if (err){
+            res.redirect("/blogs");
+        }else {
+            res.redirect("/blogs");
         }
     });
 });
